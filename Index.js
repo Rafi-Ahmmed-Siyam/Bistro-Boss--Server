ad4a544f10490cc777075fd75e8e8bf2a02a6ec4
Rafi-Ahmmed-Siyam <rafi.ahmmed920@gmail.com>
2025-07-22 01:07:52 +0600

    Added add cart API

--- Index.js
+++ Index.js
@@ -1,14 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 9000;
 
-
-// Middleware
-app.use(cors())
+// Middlewares
+app.use(cors());
 app.use(express.json());
-
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.wsg3r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -18,27 +16,54 @@ const client = new MongoClient(uri, {
       version: ServerApiVersion.v1,
       strict: true,
       deprecationErrors: true,
-   }
+   },
 });
 async function run() {
    try {
-
-      const menuCollection = client.db("BistroBoss_Collection").collection("menu");
-      const reviewCollection = client.db("BistroBoss_Collection").collection("reviews");
+      const menuCollection = client
+         .db('BistroBoss_Collection')
+         .collection('menu');
+      const reviewCollection = client
+         .db('BistroBoss_Collection')
+         .collection('reviews');
+      const cartCollection = client
+         .db('BistroBoss_Collection')
+         .collection('carts');
 
       // Get all menu data
       app.get('/menu', async (req, res) => {
-
          const result = await menuCollection.find().toArray();
          res.send(result);
-      })
+      });
 
       // Get all reviews data
       app.get('/reviews', async (req, res) => {
-
-         const result = await reviewCollection.find().toArray()
+         const result = await reviewCollection.find().toArray();
          res.send(result);
-      })
+      });
+
+      //Add cart data
+      app.post('/carts', async (req, res) => {
+         const cartItem = req.body;
+         const { menuId, email } = cartItem;
+
+         const isExist = await cartCollection.findOne({ menuId, email });
+         if (isExist) {
+            let menuId = cartItem?.menuId;
+            let email = cartItem?.email;
+            const filter = { menuId, email };
+            const update = {
+               $inc: { quantity: 1 },
+            };
+            const updateResult = await cartCollection.updateOne(filter, update);
+            return res
+               .status(200)
+               .send({ updateResult, message: 'Quantity updated!' });
+         }
+
+         const result = await cartCollection.insertOne(cartItem);
+         res.status(201).send({ result, message: 'Item added to cart!' });
+      });
 
       // Read data by food Category
       app.get('/menu/:category', async (req, res) => {
@@ -46,30 +71,31 @@ async function run() {
          const page = parseInt(req.query.page);
          const limit = parseInt(req.query.limit);
 
-         const skip = (page * limit)
+         const skip = page * limit;
 
-
-         const query = { category: category }
-         const result = await menuCollection.find(query)
+         const query = { category: category };
+         const result = await menuCollection
+            .find(query)
             .skip(skip)
             .limit(limit)
             .toArray();
-         res.send(result)
-      })
+         res.send(result);
+      });
 
       // get menu total count
       app.get('/totalMenuCount/:category', async (req, res) => {
          const category = req.params.category;
 
-
          const query = { category: category };
          const count = await menuCollection.countDocuments(query);
          res.send({ count });
-      })
+      });
 
       // await client.connect();
       // await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      console.log(
+         'Pinged your deployment. You successfully connected to MongoDB!'
+      );
    } finally {
       // Ensures that the client will close when you finish/error
       // await client.close();
@@ -78,9 +104,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-   res.send("This server is for bistro boss resturent")
+   res.send('This server is for bistro boss resturent');
 });
 
 app.listen(port, () => {
-   console.log(`My server is now running in port ${port}`)
-})
+   console.log(`My server is now running in port ${port}`);
+});
